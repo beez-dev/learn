759dee774b8323ca97f9ff1ae40490b431a407fe
beezdev <bishaljarnal456@gmail.com>
2024-10-14 19:15:01 +0545

    Update js security notes

--- vue/js_security/1.js
+++ vue/js_security/1.js
@@ -1,22 +1,3 @@
-//strict mode
-// reduce edge cases of the language
+'strict mode';
 
-
-// 'use strict';
-
-// const x = {};
-// Object.defineProperty(x, 'foo', {
-//     value: 10,
-//     writable: false
-// })
-
-// console.log(x.foo);
-// x.foo = 11;
-// console.log(x.foo)
-
-'use strict';
-
-const app = require('express')();
-const fs = require('fs');
-
-const page = fs.read
+// Reflected XSS
